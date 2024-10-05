d42a8acf4549176c0c3efe328113646fedda3109
kadeem <kingkadeem11@gmail.com>
2024-10-05 18:55:54 -0400

    linked packages to index.js and added questions

--- index.js
+++ index.js
@@ -1,7 +1,57 @@
 // TODO: Include packages needed for this application
+const fs= require("fs")
+const inquirer= require("inquirer")
+const generateMarkdown= require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message:("What is your GitHub username?"),
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message:("What is your email address?"),
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message:("What is your project's name?"),
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message:("Please write a short description of your project."),
+        name: 'description',
+    },
+    {
+        type: 'list',
+        message: ("What kind of license should your project have?"),
+        name: 'license',
+        choices: ["MIT", "ISC", "telekinesis", "l"],
+    },
+    {
+        type: 'input',
+        message:("What command should be run to install dependencies?"),
+        name: 'installtion',
+    },
+    {
+        type: 'input',
+        message:("How can the user run test?"),
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message:("How does the user use this repo?"),
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message:("How can the user contribute to this repo?"),
+        name: 'contribute',
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
